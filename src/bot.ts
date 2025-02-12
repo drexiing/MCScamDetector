a56aa9f8907b0e2a0e279e4a1dccc49d158082f8
Drex <120832806+drexiing@users.noreply.github.com>
2025-02-12 12:51:09 -0300

    Process exit if token isn't set

--- src/bot.ts
+++ src/bot.ts
@@ -2,7 +2,13 @@ import {
     Client
 } from "discord.js-selfbot-v13";
 import dotenv from "dotenv";
+import colors from "colors/safe.js";
 dotenv.config();
+
+if (!process.env.token) {
+    console.error(colors.red("Token isn't set as an environment variable."));
+    process.exit(1);
+}
 
 const client = new Client();
 
